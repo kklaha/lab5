d4d304936be8dcda6f7b6bbefe850f5821073636
kklaha <55ovsep55@gmail.com>
2025-11-14 19:00:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,34 @@ const prices = {
     mouse: 1000,
     keyboard: 3000
 };
+let quantityInput, productSelect, resultDiv, calculateBtn;
+function calculate(event) {
+    if (event) event.preventDefault();
 
-const quantityInput = document.getElementById("quantity");     
-const productSelect  = document.getElementById("product");    
-const resultDiv      = document.getElementById("result");       
-const calculateBtn   = document.getElementById("calculateBtn"); 
-
-function calculate() {
     const quantity = Number(quantityInput.value);
     const selectedProduct = productSelect.value;
     const price = prices[selectedProduct];
     if (isNaN(quantity) || quantity < 1) {
         resultDiv.textContent = "❌ Введите корректное количество!";
-        return; 
+        return;
     }
-
     const totalCost = price * quantity;
     resultDiv.innerHTML = `
         🛒 Заказ: <strong>${quantity} шт.</strong><br>
         💰 Итого: <strong>${totalCost.toLocaleString()} руб.</strong>
     `;
 }
-calculateBtn.addEventListener("click", calculate);
+function onClick(event) {
+    event.preventDefault();
+    alert("click");
+}
+window.addEventListener('DOMContentLoaded', function () {
+    console.log("DOM fully loaded and parsed");
+    quantityInput = document.getElementById("quantity");
+    productSelect = document.getElementById("product");
+    resultDiv = document.getElementById("result");
+    calculateBtn = document.getElementById("calculateBtn");
+    const myButton = document.getElementById("my-button");
+    calculateBtn.addEventListener("click", calculate);
+    myButton.addEventListener("click", onClick);
+});
